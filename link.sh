58d554d0f56e74f9980318f91e59e53568b12e62
Andrew DeMaria <ademaria@mines.edu>
2013-08-22 13:20:56 -0600

    Initial upload

---
+++ link.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# link.sh
+# Copyright (C) 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+#
+# All Rights Reserved.
+#
+
+if [ -d "$1" ]; 
+then
+	dir=`readlink -f "$1"`
+	ln -sf "$dir/gitconfig" ~/.gitconfig
+	ln -sf "$dir/gitignore_global" ~/.gitignore_global
+else
+	exit 1
+fi
+
